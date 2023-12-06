1a2315c764f2c5c2c2a10dd9cd7504dcc0310d09
pw-sdk-test <151908281+pw-sdk-test@users.noreply.github.com>
2023-12-06 19:20:45 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ from paymentwall.base import Paymentwall
 from paymentwall.product import Product
 from paymentwall.widget import Widget
 from paymentwall.pingback import Pingback
+from paymentwall.response.err import Err
